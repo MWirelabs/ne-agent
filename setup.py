b0051653313ddf91a50d84a7f4d693ea8d5d743a
MWire Labs <badnyal@gmail.co>
2026-06-28 00:20:41 +0000

    v0.1.4: fix package_data with MANIFEST.in

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ne-agent",
-    version="0.1.3",
+    version="0.1.4",
     author="MWire Labs",
     author_email="connect@mwirelabs.com",
     description="First open-source AI agent for Northeast Indian languages",
